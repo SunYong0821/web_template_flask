e46673464ffe21eda4e72655334b6b77db542841
skenoy <skenoy@gmail.com>
2020-02-14 12:36:23 +0800

    add a redirect of home

--- app/home/__init__.py
+++ app/home/__init__.py
@@ -1,6 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 home = Blueprint('home', __name__)
+
+@home.route('/')
+def honme():
+    return redirect(url_for('home.index'))
 
 @home.route('/index')
 def index():
